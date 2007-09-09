11dda7345920d78d185560144fb16db156d0e212
rajesh.dhawan <rajesh.dhawan@0272f574-9338-0410-9dda-895bf7a4d7d8>
2007-09-09 18:36:48 +0000

    - Retrying branch

--- branches/rajesh-sandbox/trunk/envMac.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PROJECTPATH=/Users/$USER/Desktop
-PROJECT=qs
-export DJANGO_SETTINGS_MODULE="$PROJECT".settings
-export PYTHONPATH=$PROJECTPATH:$PYTHONPATH:
-cd $PROJECTPATH/$PROJECT
-export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.4/lib/python2.4/site-packages/django/bin/
