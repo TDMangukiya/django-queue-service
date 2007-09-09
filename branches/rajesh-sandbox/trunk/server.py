579dcf2fab168a1f26ede5802c568cd2ceefaeca
rajesh.dhawan <rajesh.dhawan@0272f574-9338-0410-9dda-895bf7a4d7d8>
2007-09-09 18:34:56 +0000

    - Created Sandbox for Rajesh - Based off of r11

---
+++ branches/rajesh-sandbox/trunk/server.py
@@ -0,0 +1,20 @@
+from django.core.handlers.wsgi import WSGIHandler
+import wsgiserver
+
+if __name__ == '__main__':
+    wsgi_apps = [('/', WSGIHandler())]
+    # server = wsgiserver.CherryPyWSGIServer(('localhost', 8000), wsgi_apps, server_name='localhost')
+    server = wsgiserver.CherryPyWSGIServer(('localhost', 8000), WSGIHandler(), server_name='localhost')
+    # 
+    # # Want SSL support? Just set these attributes
+    # # server.ssl_certificate = <filename>
+    # # server.ssl_private_key = <filename>
+    # 
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        server.stop()
+
+    # from wsgiref.simple_server import make_server
+    # httpd = make_server('', 8000, WSGIHandler())
+    # httpd.serve_forever()
