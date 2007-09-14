e86c5e386024af084ec2b6e92478bf90f4e71cde
rajesh.dhawan <rajesh.dhawan@0272f574-9338-0410-9dda-895bf7a4d7d8>
2007-09-14 20:40:57 +0000

    - Moved templates and urls into queue app directory - Created templatetags under queue - Created a docs directory - Added auto setup of DJANGO_SETTINGS_MODULE environment variable to server.py

--- branches/rajesh-sandbox/server.py
+++ branches/rajesh-sandbox/server.py
@@ -1,6 +1,11 @@
 from django.core.handlers.wsgi import WSGIHandler
 import wsgiserver
 
+import os
+
+if not os.environ.get('DJANGO_SETTINGS_MODULE', False):
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'qs.settings'
+
 if __name__ == '__main__':
     wsgi_apps = [('/', WSGIHandler())]
     # server = wsgiserver.CherryPyWSGIServer(('localhost', 8000), wsgi_apps, server_name='localhost')
--- branches/rajesh-sandbox/urls.py
+++ branches/rajesh-sandbox/urls.py
@@ -1,20 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^qs/', include('qs.foo.urls')),
-    (r'^createqueue/', 'qs.queue.views.create_queue'), #post 'name' of queue
-    (r'^deletequeue/', 'qs.queue.views.delete_queue'), #post 'name' of queue
-    (r'^purgequeue/', 'qs.queue.views.purge_queue'),   #post 'name' of queue
-    (r'^listqueues/', 'qs.queue.views.list_queues'),
-    
-    (r'^q/(?P<queue_name>\w+)/put/', 'qs.queue.views.put'),
-    (r'^q/(?P<queue_name>\w+)/clearexpire/', 'qs.queue.views.clear_expirations'),
-    (r'^q/(?P<queue_name>\w+)/count/json/', 'qs.queue.views.count', {"response_type":"json"}),
-    (r'^q/(?P<queue_name>\w+)/count/', 'qs.queue.views.count', {"response_type":"text"}),
-    (r'^q/(?P<queue_name>\w+)/delete/$', 'qs.queue.views.delete'), #post 'message_id' of msg
-    (r'^q/(?P<queue_name>\w+)/json/', 'qs.queue.views.get', {"response_type":"json"}),
-    (r'^q/(?P<queue_name>\w+)/', 'qs.queue.views.get', {"response_type":"text"}),
+    (r'^', include('qs.queue.urls')), # Everything goes to the Queue app at the moment
     
     # Uncomment this for admin:
     # (r'^admin/', include('django.contrib.admin.urls')),
