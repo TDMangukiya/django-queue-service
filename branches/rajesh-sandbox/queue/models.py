0d087ff21f45b95aa9a0b685dac92ad0eac0127a
rajesh.dhawan <rajesh.dhawan@0272f574-9338-0410-9dda-895bf7a4d7d8>
2007-09-14 19:01:18 +0000

    * added support for q.message_set.pop() (where q is an instance) * message manager now supports queue instances as well as queue names * clear_expirations now raises Queue.DoesNotExist when given a bad queue name * purge now deletes a queue's messages without looping * added a couple of new test cases on a different queue than 'default'

--- branches/rajesh-sandbox/queue/models.py
+++ branches/rajesh-sandbox/queue/models.py
@@ -18,26 +18,36 @@ class Queue(models.Model):
     class Admin:
         pass
     
-# Create your models here.
 class MessageManager(models.Manager):
-    def pop(self, queue_name, expire_interval=5):
+    def pop(self, queue=None, expire_interval=5):
         """ returns a visible Message if available, or None. Any Message
         returned is set to 'invisible', so that future pop() invocations won't 
-        retrieve it until after an expiration time (default of 5 minutes)."""
+        retrieve it until after an expiration time (default of 5 minutes).
+        
+        queue can either be the name of a queue or an instance of Queue.
+        """
         try:
-            result = self.filter(queue__name=queue_name, visible=True).order_by('timestamp')[0:1].get()
+            if queue is None:
+                # this allows is to do:
+                #   q.message_set.pop() if we already have an instance of q at hand
+                f = self
+            else:
+                f = isinstance(queue, Queue) and queue.message_set or \
+                                             self.filter(queue__name=queue)
+            result = f.filter(visible=True).order_by('timestamp', 'id')[0:1].get()
             result.visible = False
-            result.expires = datetime.datetime.now()+datetime.timedelta(minutes=expire_interval)
+            result.expires = datetime.datetime.now() + datetime.timedelta(minutes=expire_interval)
             result.save()
             return result
         except Message.DoesNotExist:
             return None
 
-    def clear_expirations(self,queue_name):
-        try:
-            q = Queue.objects.get(name=queue_name)
-        except Queue.DoesNotExist:
-            return None
+    def clear_expirations(self, queue):
+        """
+        Changes visibility to True for messages whose expiration time has elapsed.
+        queue can either be the name of a queue or an instance of Queue.
+        """
+        q = isinstance(queue, Queue) and queue or Queue.objects.get(name=queue)
         from django.db import connection, transaction, DatabaseError
         cursor = connection.cursor()
         try:
@@ -46,7 +56,8 @@ class MessageManager(models.Manager):
                        expires < %%s" % self.model._meta.db_table, 
                        [None, True, q.id, False, datetime.datetime.now()])
         except DatabaseError:
-            # thread safety: updates can fail silently
+            # For thread safety: these updates could be allowed to fail silently
+            # Perhaps, this isn't needed.
             pass
         else:
             transaction.commit_unless_managed()
@@ -60,7 +71,9 @@ class Message(models.Model):
     default
     >>> default.message_set.count()
     3
+    >>> test2 = Queue.objects.create(name='test2')
     >>> from queue.models import Message
+    >>> test2msg = test2.message_set.create(message='test2 message')
     >>> x=Message(message="t1",queue=default)
     >>> x.save()
     >>> x=Message(message="t2",queue=default)
@@ -69,14 +82,14 @@ class Message(models.Model):
     >>> x.save()
     >>> print Message.objects.pop('default').message
     a
-    >>> print Message.objects.pop('default').message
+    >>> print Message.objects.pop(default).message
     b
     >>> y=Message.objects.pop('default')
     >>> print y.message
     c
     >>> y.visible
     False
-    >>> print len(Message.objects.all())
+    >>> print len(default.message_set.all())
     6
     >>> x=Message(message="hello",queue=default)
     >>> x.save()
@@ -93,7 +106,7 @@ class Message(models.Model):
     >>> x.visible
     True
     
-    >>> len(Message.objects.filter(visible=True))
+    >>> len(Message.objects.filter(visible=True, queue=default))
     5
     >>> x=Message(message="aa",queue=default).save()
     >>> x=Message(message="bb",queue=default).save()
@@ -107,13 +120,20 @@ class Message(models.Model):
     >>> b.save()
     >>> import time
     >>> time.sleep(.02) # needed on fast machines for the below test to succeed
-    >>> Message.objects.clear_expirations('default')
-    >>> len(Message.objects.filter(visible=True))
+    >>> Message.objects.clear_expirations(default)
+    >>> len(default.message_set.filter(visible=True))
     7
+    >>> t2x = test2.message_set.pop()
+    >>> print t2x.message
+    test2 message
+    >>> print t2x.visible, t2x.expires is not None
+    False True
     """
     message = models.TextField()
     visible = models.BooleanField(default=True, db_index=True)
-    expires = models.DateTimeField(null=True, blank=True, db_index=True)
+    expires = models.DateTimeField(null=True, blank=True, db_index=True,
+                help_text="After this time has elapsed, the visibility of the message \
+                           is changed back from False to True (when clear_expirations is executed).")
     timestamp = models.DateTimeField(null=True, blank=True, db_index=True, default=datetime.datetime.now)
     queue = models.ForeignKey(Queue, raw_id_admin=True)
     objects = MessageManager()
@@ -124,7 +144,7 @@ class Message(models.Model):
         super(Message, self).save()
     
     def __str__(self):
-        return "QM<%s> : %s" % (self.id,self.message)
+        return "QM<%s> : %s" % (self.id, self.message)
             
     class Admin:
         pass
