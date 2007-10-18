3647a81db0cd80ab7516e1331401c6b7c9f212f2
joseph.heck <joseph.heck@0272f574-9338-0410-9dda-895bf7a4d7d8>
2007-10-18 22:52:16 +0000

    removing the branch - will resnap and generate again from the current trunk

--- branches/unstable/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    (r'^', include('qs.queue.urls')), # Everything goes to the Queue app at the moment
-    
-    # Uncomment this for admin:
-    # (r'^admin/', include('django.contrib.admin.urls')),
-)
