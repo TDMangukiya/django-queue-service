e41c3d8c99f8b00c2682b1877b6e3157f702fa8a
joseph.heck <joseph.heck@0272f574-9338-0410-9dda-895bf7a4d7d8>
2007-10-19 17:02:28 +0000

    code re-org to support 'qs' explicitly and enable space outside the code itself for docs, scripts, binaries, etc.

---
+++ trunk/qs/queue/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    (r'^createqueue/$', 'qs.queue.views.create_queue'), #post 'name' of queue
+    (r'^deletequeue/$', 'qs.queue.views.delete_queue'), #post 'name' of queue
+    (r'^purgequeue/$', 'qs.queue.views.purge_queue'),   #post 'name' of queue
+    (r'^listqueues/$', 'qs.queue.views.list_queues'),
+    
+    (r'^q/(?P<queue_name>\w+)/put/$', 'qs.queue.views.put'),
+    (r'^q/(?P<queue_name>\w+)/clearexpire/$', 'qs.queue.views.clear_expirations'),
+    (r'^q/(?P<queue_name>\w+)/count/json/$', 'qs.queue.views.count', {"response_type":"json"}),
+    (r'^q/(?P<queue_name>\w+)/count/$', 'qs.queue.views.count', {"response_type":"text"}),
+    (r'^q/(?P<queue_name>\w+)/delete/$', 'qs.queue.views.delete'), #post 'message_id' of msg
+    (r'^q/(?P<queue_name>\w+)/json/$', 'qs.queue.views.get', {"response_type":"json"}),
+    (r'^q/(?P<queue_name>\w+)/$', 'qs.queue.views.get', {"response_type":"text"}),
+)
