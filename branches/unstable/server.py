90949878f3193021c8640d6564569037582c76bc
rajesh.dhawan <rajesh.dhawan@0272f574-9338-0410-9dda-895bf7a4d7d8>
2008-07-01 20:39:11 +0000

    Updated CherryPy WSGIServer to latest version

--- branches/unstable/server.py
+++ branches/unstable/server.py
@@ -1,7 +1,6 @@
-from django.core.handlers.wsgi import WSGIHandler
-import wsgiserver
-
 import os
+import wsgiserver
+from django.core.handlers.wsgi import WSGIHandler
 
 if not os.environ.get('DJANGO_SETTINGS_MODULE', False):
     os.environ['DJANGO_SETTINGS_MODULE'] = 'qs.settings'
@@ -12,10 +11,14 @@ _HOST = getattr(settings, 'DQS_HOST', 'localhost')
 _PORT = getattr(settings, 'DQS_PORT', 8000)
 _SERVER_NAME = getattr(settings, 'DQS_SERVER_NAME', 'localhost')
 
+
 def runserver():
-    wsgi_apps = [('/', WSGIHandler())]
-    # server = wsgiserver.CherryPyWSGIServer(('localhost', 8000), wsgi_apps, server_name='localhost')
-    server = wsgiserver.CherryPyWSGIServer((_HOST, int(_PORT)), WSGIHandler(), server_name=_SERVER_NAME)
+    wsgi_apps = WSGIPathInfoDispatcher([('/', WSGIHandler())])
+    server = CherryPyWSGIServer((_HOST, int(_PORT)),
+                wsgi_apps,
+                numthreads=numthreads,
+                server_name=_SERVER_NAME)
+
     # 
     # # Want SSL support? Just set these attributes
     # # server.ssl_certificate = <filename>
